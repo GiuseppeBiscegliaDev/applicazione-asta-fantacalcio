8021148b066b0db733d9afd9ac26ead5fab600cb
Giuseppe Bisceglia <giuseppebisceglia.dev@gmail.com>
2025-08-07 13:55:02 +0200

    fix(user-service): Risolto il collegamento tra user-service e API Gateway

--- backend/user-service/src/main/java/com/fantacalcio/user_service/config/SecurityConfig.java
+++ backend/user-service/src/main/java/com/fantacalcio/user_service/config/SecurityConfig.java
@@ -12,11 +12,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user/ping").permitAll()
-                        .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/user/ping").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
